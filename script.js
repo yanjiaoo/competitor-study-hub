7c07f45bd6d58b420ba39a133155bc3a2852f8b3
yanjiaoo <yanjiaoo@amazon.com>
2026-05-14 20:06:30 +0800

    图表Y轴：海运1000-8000步长1000，空运3-10步长1

--- script.js
+++ script.js
@@ -1257,7 +1257,7 @@ function drawFreightCharts(data) {
             maintainAspectRatio: true,
             aspectRatio: 2,
             plugins: { legend: { position: 'bottom', labels: { font: { size: 12 } } } },
-            scales: { y: { title: { display: true, text: '$/FEU (40尺柜)' }, min: 1000 } }
+            scales: { y: { title: { display: true, text: '$/FEU (40尺柜)' }, min: 1000, max: 8000, ticks: { stepSize: 1000 } } }
         }
     });
 
@@ -1282,7 +1282,7 @@ function drawFreightCharts(data) {
             maintainAspectRatio: true,
             aspectRatio: 2,
             plugins: { legend: { position: 'bottom', labels: { font: { size: 12 } } } },
-            scales: { y: { title: { display: true, text: '$/kg' }, min: 3 } }
+            scales: { y: { title: { display: true, text: '$/kg' }, min: 3, max: 10, ticks: { stepSize: 1 } } }
         }
     });
 
